33d469d8634940f8124031578df93f3b2e6ae659
jomar godinez <jomargodinez70@gmail.com>
2025-01-24 14:37:02 +0800

    Update useFormatters.js

--- src/composables/useFormatters.js
+++ src/composables/useFormatters.js
@@ -1,12 +1,11 @@
-import { format } from "date-fns";
 
 export function useFormatters() {
     const formatDateWithTime = (date) => {
-        return format(new Date(date), "MMM d, yyyy h:mm a");
+        return date;
     };
 
     const formatDateOnly = (date) => {
-        return format(new Date(date), 'yyyy-MM-dd')
+        return date
     }
 
     return {
