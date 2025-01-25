80cf97867eee611eeb1de8549cecc2c43bb36c41
jomar godinez <jomargodinez70@gmail.com>
2025-01-25 14:41:34 +0800

    clean:  cleaning some code related to checking and  disable multiple request at once

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,18 @@ import { RouterLink } from 'vue-router'
 import { useAuth } from '@/stores/auth';
 
 const auth = useAuth();
+
 </script>
 <template>
   <body class="bg-gradient-to-br from-blue-50 to-blue-100 min-h-screen flex flex-col">
     <header>
       <nav class="bg-white shadow-sm mx-auto px-24 py-5">
           <div class="container mx-auto flex justify-between items-center">
-              <div class="text-2xl font-bold text-blue-600">Sprint Sync</div>
+              <div class="text-2xl font-bold text-blue-600">SprintSync</div>
               <div class="space-x-4">
-                  <RouterLink to="#" class="text-gray-700 hover:text-blue-600">Features</RouterLink>
-                  <RouterLink to="#" class="text-gray-700 hover:text-blue-600">Pricing</RouterLink>
-                  <button @click="auth.logout" class="text-gray-700 hover:text-blue-600">Logout</button>
+                <RouterLink to="#" class="text-gray-700 hover:text-blue-600">Features</RouterLink>
+                <RouterLink to="#" class="text-gray-700 hover:text-blue-600">Pricing</RouterLink>
+                <button v-if="auth.check" @click="auth.logout" class="text-gray-700 hover:text-blue-600">Logout</button>
               </div>
           </div>
       </nav>
@@ -24,7 +25,7 @@ const auth = useAuth();
     </main>
     <footer class="bg-white py-6 border-t">
         <div class="container mx-auto text-center text-gray-600">
-            © 2025 Sprint Sync. All rights reserved.
+            © 2025 SprintSync. All rights reserved.
         </div>
     </footer>
   </body>
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useAuth } from "@/stores/auth";
 
 window.axios = axios;
- 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.baseURL = import.meta.env.VITE_API_URL;
