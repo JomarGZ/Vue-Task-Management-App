a48f1f2647e653475d84b9e1df8c0f5005adafa0
jomar godinez <jomargodinez70@gmail.com>
2025-01-26 11:50:11 +0800

    feat: changing the changing the navigation sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -54,74 +54,66 @@ const router = createRouter({
           name: 'organization.dashboard',
           component: () => import('@/views/Dashboards/OrganizationDashboard.vue'),
         },
+        {
+          path: '/profile',
+          name: 'profile.index',
+          component: () => import('@/views/Auth/ProfileView.vue'),
+        },
+        {
+          path: '/tasks/create',
+          name: 'tasks.create',
+          component: () => import('@/views/Tasks/CreateView.vue'),
+        },
+        {
+          path: '/tasks',
+          name: 'tasks.index',
+          component: () => import('@/views/Tasks/IndexView.vue'),
+        },
+        {
+          path: '/tasks/:id/edit',
+          name: 'tasks.edit',
+          component: () => import('@/views/Tasks/EditView.vue'),
+        },
+        {
+          path: '/tasks/:id',
+          name: 'tasks.show',
+          component: () => import('@/views/Tasks/ShowView.vue'),
+        },
+        {
+          path: '/members',
+          name: 'members.index',
+          component: () => import('@/views/Members/IndexView.vue'),
+        },
+        {
+          path: '/members/create',
+          name: 'members.create',
+          component: () => import('@/views/Members/CreateView.vue'),
+        },
+        {
+          path: '/members/:id',
+          name: 'members.show',
+          component: () => import('@/views/Members/ShowView.vue'),
+        },
+        {
+          path: '/projects',
+          name: 'projects.index',
+          component: () => import('@/views/Projects/IndexView.vue'),
+        },
+        {
+          path: '/projects/create',
+          name: 'projects.create',
+          component: () => import('@/views/Projects/CreateView.vue'),
+        },
+        {
+          path: '/projects/:id',
+          name: 'projects.show',
+          component: () => import('@/views/Projects/ShowView.vue'),
+        },
       ]
     },
-    {
-      path: '/profile',
-      name: 'profile.index',
-      beforeEnter: auth,
-      component: import('@/views/Auth/ProfileView.vue'),
-    },
-    {
-      path: '/tasks/create',
-      name: 'tasks.create',
-      beforeEnter: auth,
-      component: () => import('@/views/Tasks/CreateView.vue'),
-    },
-    {
-      path: '/tasks',
-      name: 'tasks.index',
-      beforeEnter: auth,
-      component: () => import('@/views/Tasks/IndexView.vue'),
-    },
-    {
-      path: '/tasks/:id/edit',
-      name: 'tasks.edit',
-      beforeEnter: auth,
-      component: () => import('@/views/Tasks/EditView.vue'),
-    },
-    {
-      path: '/tasks/:id',
-      name: 'tasks.show',
-      beforeEnter: auth,
-      component: () => import('@/views/Tasks/ShowView.vue'),
-    },
-    {
-      path: '/members',
-      name: 'members.index',
-      beforeEnter: auth,
-      component: () => import('@/views/Members/IndexView.vue'),
-    },
-    {
-      path: '/members/create',
-      name: 'members.create',
-      beforeEnter: auth,
-      component: () => import('@/views/Members/CreateView.vue'),
-    },
-    {
-      path: '/members/:id',
-      name: 'members.show',
-      beforeEnter: auth,
-      component: () => import('@/views/Members/ShowView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects.index',
-      beforeEnter: auth,
-      component: () => import('@/views/Projects/IndexView.vue'),
-    },
-    {
-      path: '/projects/create',
-      name: 'projects.create',
-      beforeEnter: auth,
-      component: () => import('@/views/Projects/CreateView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'projects.show',
-      beforeEnter: auth,
-      component: () => import('@/views/Projects/ShowView.vue'),
-    },
+ 
+    
+   
   ],
 })
  
