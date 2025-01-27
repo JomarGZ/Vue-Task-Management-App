0b1f5a250e4cc24d4564c1dfee27f390a42555c2
jomar godinez <jomargodinez70@gmail.com>
2025-01-27 13:17:42 +0800

    feat: adding search functionality

--- src/stores/projectStore.js
+++ src/stores/projectStore.js
@@ -1,13 +1,28 @@
+import { useSweetAlert } from "@/composables/useSweetAlert2";
+import debounce from "lodash.debounce";
 import { defineStore } from "pinia";
 import { reactive, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 export const useProjectStore = defineStore("project", () => {
     const errors = reactive({});
     const projects = ref(null);
+    const router = useRouter();
+    const route = useRoute();
+    const searchInput = ref(route.query.search || '');
+    const {showToast, showConfirmDialog} = useSweetAlert();
+    const loading = ref(false);
     const form = reactive({
         name: '',
         description: ''
     });
+
+    const resetForm = () => {
+        form.name = '';
+        form.description = '';
+
+        errors.value = {}
+    }
 
     const pagination = reactive({
         current_page: 1,
@@ -17,10 +32,43 @@ export const useProjectStore = defineStore("project", () => {
         to: 0,
         total: 0,
       })
-
+      const changePage = (pageUrl) => {
+        if (!pageUrl) return
+  
+        const params = new URLSearchParams(new URL(pageUrl).search)
+        const page = params.get('page')
+  
+        if (page) {
+            updateQueryAndFetchPosts({ page: page })
+        }
+    }
+    const debounceSearch = debounce((newSearch) => {
+        router
+        .replace({
+            query: newSearch ? { search: newSearch } : {},
+        })
+        .then(() => {
+            getProjects()
+        })
+    }, 300)
+    const orderBy = (column = 'created_at') => {
+        const toggleDirection = route.query.direction === 'desc' ? 'asc' : 'desc'
+        updateQueryAndFetchPosts({ column, direction: toggleDirection })
+    }
+    const updateQueryAndFetchPosts = (params = {}) => {
+        router
+          .push({
+            query: {
+              ...route.query,
+              ...params,
+            },
+          })
+          .then(() => getProjects())
+      }
+  
     const getProjects = () => {
         return window.axios
-            .get("v1/projects")
+            .get("v1/projects", {params: route.query})
             .then((response) => {
                 const projectsData = response?.data;
                 if (projectsData) {
@@ -37,8 +85,38 @@ export const useProjectStore = defineStore("project", () => {
                 console.error('Error on fetching projects:', error)
             })
     }
+
+    const handleSubmit = async () => {
+        if(loading.value) return;
+
+        loading.value = true;
+
+        errors.value = {};
+        return window.axios
+            .post("v1/projects", form)
+            .then(response => {
+                showToast("Project Added Successfully");
+                resetForm()
+                router.push({name: 'projects.index'})
+            })
+            .catch(error => {
+                if (error.response.status === 422) {
+                    errors.value = error.response.data.errors;
+                } else {
+                    console.error('Error on adding project:', error)
+                }
+            })
+            .finally(() => loading.value = false);
+    }
     return {
         getProjects,
+        handleSubmit,
+        resetForm,
+        changePage,
+        debounceSearch,
+        orderBy,
+        searchInput,
+        loading,
         projects,
         pagination,
         errors,
