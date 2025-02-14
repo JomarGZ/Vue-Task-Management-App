e67c256eebb27f03a979ec76a17a6ae5f0fe5745
jomar godinez <jomargodinez70@gmail.com>
2025-02-14 14:34:21 +0800

    feat: adding loading indicator for fetching members

--- src/stores/memberStore.js
+++ src/stores/memberStore.js
@@ -9,6 +9,8 @@ export const useMemberStore = defineStore("memberStore", () => {
   const errors = reactive({});
   const loading = ref(false);
   const router = useRouter();
+  const isFetchLoading = ref(false);
+  const isFetchError = ref(false);
   const route = useRoute();
   const memberList = ref(null);
   const members = ref(null);
@@ -91,23 +93,28 @@ export const useMemberStore = defineStore("memberStore", () => {
   }
 
   const getMembers = async () => {
-      return window.axios
-          .get("v1/tenant/members", { params:  route.query })
-          .then((response) => {
-              const membersData = response?.data
-              if (membersData) {
-                members.value = membersData.data
-                pagination.current_page = membersData.meta.current_page || 1
-                pagination.links = membersData.meta?.links || {}
-                pagination.last_page = membersData.meta?.last_page || 0
-                pagination.total = membersData.meta?.total || 0
-                pagination.from = membersData.meta?.from || 0
-                pagination.to = membersData.meta?.to || 0
-              }
-          })
-          .catch((error) => {
-              console.error('Error on fetching members', error)
-          })
+    if (isFetchLoading.value) return;
+    isFetchLoading.value = true;
+    isFetchError.value = false;
+
+    return window.axios
+        .get("v1/tenant/members", { params:  route.query })
+        .then((response) => {
+            const membersData = response?.data
+            if (membersData) {
+              members.value = membersData.data
+              pagination.current_page = membersData.meta.current_page || 1
+              pagination.links = membersData.meta?.links || {}
+              pagination.last_page = membersData.meta?.last_page || 0
+              pagination.total = membersData.meta?.total || 0
+              pagination.from = membersData.meta?.from || 0
+              pagination.to = membersData.meta?.to || 0
+            }
+        })
+        .catch((error) => {
+            isFetchError.value = true;
+            console.error('Error on fetching members', error)
+        }).finally(() => isFetchLoading.value = false);
   }
 
   const getMemberListWithoutPagination = async () => {
@@ -155,6 +162,8 @@ export const useMemberStore = defineStore("memberStore", () => {
       deleteMember,
       getMemberListWithoutPagination,
       debounceSearch,
+      isFetchLoading,
+      isFetchError,
       memberList,
       pagination,
       searchInput,
