496a14c21c08b665639b52a1110afe821c5b3b8a
jomar godinez <jomargodinez70@gmail.com>
2025-02-20 16:03:05 +0800

    design: change structural design

--- src/components/CommentBlock.vue
+++ src/components/CommentBlock.vue
@@ -2,12 +2,13 @@
 import { formatDateDistance } from '@/composables/useFormatters';
 import CommentOption from './CommentOption.vue';
 import { useTaskComments } from '@/stores/taskCommentStore';
+import { ref } from 'vue';
 const props = defineProps({
     comment: Object,
     author: Object
 })
 const taskComment = useTaskComments();
-
+const isOpen = ref(false);
 const handleDelete = async () => {
      await taskComment.handleDeleteComment({id: props.comment?.id});
 }
@@ -21,10 +22,32 @@ const emit = defineEmits(['clicked-reply']);
                 <span class="text-sm text-gray-500">Product Manager</span>
                 <span class="text-sm text-gray-400">• {{ formatDateDistance(comment?.created_at) }}</span>
             </div>
-            <div class="flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity">
-                <button @click="handleDelete" class="p-1 hover:bg-gray-200 rounded-full transition-colors" title="delete">
-                    <IconSVG name="x-svg" class="w-4 h-4 text-gray-500"/>
+            <div class="relative inline-block opacity-0 group-hover:opacity-100 transition-opacity" ref="dropdownRef">
+                <button
+                    @click="isOpen = !isOpen"
+                    class="p-1 hover:bg-gray-200 rounded-full transition-colors"
+                >
+                    <IconSVG name="three-dot-svg"  class="w-4 h-4 text-gray-500"/>
                 </button>
+                <div
+                v-if="isOpen"
+                class="absolute right-0 mt-2 w-36 bg-white rounded-md shadow-lg border border-gray-200 z-50"
+                >
+                    <div class="py-1">
+                        <button
+                            @click="handleEdit"
+                            class="w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 text-left flex items-center"
+                            >
+                            <IconSVG name="edit-svg" class="w-4 h-4"/>
+                        </button>
+                            <button
+                            @click="handleDelete"
+                            class="w-full px-4 py-2 text-sm text-red-600 hover:bg-gray-100 text-left flex items-center"
+                            >
+                            <IconSVG name="trash-svg"  class="w-4 h-4"/>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
         <p class="mt-2 text-gray-700">{{ comment?.content }}</p>
--- src/components/icons/Icon.vue
+++ src/components/icons/Icon.vue
@@ -89,4 +89,9 @@ defineProps(['name']);
             <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
         </svg>
     </template>
+    <template v-else-if="name === 'three-dot-svg'">
+        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+        </svg>
+    </template>
 </template>
