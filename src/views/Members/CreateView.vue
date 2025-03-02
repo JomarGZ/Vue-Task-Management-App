bbd7086a281ad99b1c3ea7f5a53044aef0f9d9a2
jomar godinez <jomargodinez70@gmail.com>
2025-03-02 18:02:52 +0800

    refactor: move the button code to its own component

--- src/views/Members/CreateView.vue
+++ src/views/Members/CreateView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import BaseButton from '@/components/forms/BaseButton.vue';
 import InputField from '@/components/forms/InputField.vue';
 import { useMemberStore } from '@/stores/memberStore';
 import { onBeforeUnmount } from 'vue';
@@ -155,18 +156,13 @@ onBeforeUnmount(store.resetForm)
                     <RouterLink :to="{name: 'members.index'}" class="px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                         Cancel
                     </RouterLink>
-                    <button 
-                        :disabled="store.isActionDisabled" 
-                        type="submit" 
-                        :class="{
-                        'px-6 py-3 flex items-center justify-center gap-2 text-white rounded-lg font-medium': true,
-                        'hover:bg-indigo-700 bg-indigo-600':! store.isActionDisabled,
-                        'bg-indigo-300': store.isActionDisabled 
-                        }"
-                    >
-                        <IconSpinner name="white-spinner" v-if="store.loading"/>
-                        Create Task
-                    </button>
+                   <BaseButton
+                        :isActionDisabled="store.isActionDisabled"
+                        type="submit"
+                        :isLoading="store.loading"
+                   >
+                        Add Member
+                    </BaseButton>
                 </div>
             </form>
         </div>
